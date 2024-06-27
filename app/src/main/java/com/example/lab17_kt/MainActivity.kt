bbf46481efc0bc3cfb651fbcf2a90f2e01d489bc
Sivan Chen <ryan1825114@gmail.com>
2024-06-27 17:02:00 +0800

    ListActivity() with AdapterView.OnItemClickListener

--- app/src/main/java/com/example/lab17_kt/MainActivity.kt
+++ app/src/main/java/com/example/lab17_kt/MainActivity.kt
@@ -1,22 +1,72 @@
 package com.example.lab17_kt
 
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+import android.app.ListActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+
+
+class MainActivity : ListActivity(), AdapterView.OnItemClickListener {
+    private var m_incomeDescArray: Array<String>? = null
+    private var m_idArray: Array<Long>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // 取得這個ListActivity的ListView
+        val lv = listView
+
+        // 從XML生成標題的View
+        val headerView: View = layoutInflater.inflate(
+            R.layout.income_header,
+            findViewById<LinearLayout>(R.id.income_header)
+        )
+
+        // 從XML生成標尾的View
+        val footerView: View = layoutInflater.inflate(
+            R.layout.income_footer,
+            findViewById<LinearLayout>(R.id.income_footer)
+        )
+        lv.addHeaderView(headerView)
+        lv.addFooterView(footerView)
+        readFromSQLite()
+
+    }
+
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+    }
+
+    private fun readFromSQLite() {
+        val helper = DBHelper(this)
+        val db = helper.readableDatabase
+        val incomeStringList: MutableList<String> = ArrayList()
+        val idList: MutableList<Long> = ArrayList()
+        val cursor = db.query(
+            "INCOME_MAIN", arrayOf(
+                "_ID",
+                "INCOME_DESCRIPTION"
+            ), null, null, null, null, null
+        )
+        try {
+            if (cursor.moveToFirst()) {
+                do {
+                    val id = cursor.getLong(0)
+                    val incomeString = cursor.getString(1)
+                    incomeStringList.add(incomeString)
+                    idList.add(id)
+                } while (cursor.moveToNext())
+            }
+        } finally {
+            cursor.close()
         }
-        val tryDBHelper = DBHelper(this)
-        val db = tryDBHelper.writableDatabase
+        // 設定內容的字串陣列
+        m_incomeDescArray = incomeStringList.toTypedArray()
+        // 設定索引的長整數陣列
+        m_idArray = idList.toTypedArray()
+        db.close()
     }
 }
